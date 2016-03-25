a6510dd19274d3ee8657aed78ecd72a03bea8653
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-03-25 13:55:06 +0100

    Adds the tap gesture recognizer

--- Example/Ripple/Ripple/ViewController.swift
+++ Example/Ripple/Ripple/ViewController.swift
@@ -3,10 +3,23 @@ import Ripple
 
 class ViewController: UIViewController {
 
+  lazy var tapGesture: UITapGestureRecognizer = { [unowned self] in
+    let gesture = UITapGestureRecognizer()
+    gesture.addTarget(self, action: #selector(handleTapGesture))
+
+    return gesture
+  }()
+
   override func viewDidLoad() {
     super.viewDidLoad()
 
     view.backgroundColor = UIColor(red:0.12, green:0.57, blue:0.96, alpha:1.00)
+  }
+
+  // MARK: - Action methods
+
+  func handleTapGesture() {
+    print("Hey")
   }
 
   // MARK: - Helper methods

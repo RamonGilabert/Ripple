b98dae53531fc9618f833fdc78923aedce427f67
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-03-25 13:44:10 +0100

    Adds the skeleton of the first view

--- Example/Ripple/Ripple/ViewController.swift
+++ Example/Ripple/Ripple/ViewController.swift
@@ -1,8 +1,17 @@
 import UIKit
+import Ripple
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    view.backgroundColor = UIColor(red:0.12, green:0.57, blue:0.96, alpha:1.00)
+  }
+
+  // MARK: - Helper methods
+
+  override func preferredStatusBarStyle() -> UIStatusBarStyle {
+    return .LightContent
   }
 }
---
+++ Sources/Ripple.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+public struct Ripple {
+
+  public static func ripple() {
+    print("Sup")
+  }
+}

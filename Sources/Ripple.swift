647ea7bee298bf25c4e333438c0f0fdb002f23f2
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-03-25 18:05:50 +0100

    Improves the ripple effect

--- Sources/Ripple.swift
+++ Sources/Ripple.swift
@@ -1,17 +1,25 @@
 import UIKit
 
-public func ripple(center: CGPoint, view: UIView) {
+public func ripple(center: CGPoint, view: UIView,
+                   size: CGFloat = 75, multiplier: CGFloat = 4,
+                   color: UIColor = UIColor.whiteColor()) {
 
-  let ripple = Ripple()
+  let ripple = Ripple(size: CGSize(width: size, height: size), multiplier: multiplier, color: color)
   ripple.activate(center, view: view)
 }
 
 class Ripple: NSObject {
 
-  let size = CGSizeMake(50, 50)
-  let multiplier: CGFloat = 4
-
+  var size: CGSize
+  var multiplier: CGFloat
+  var color: UIColor
   var ripples: [UIView] = []
+
+  init(size: CGSize, multiplier: CGFloat, color: UIColor) {
+    self.size = size
+    self.multiplier = multiplier
+    self.color = color
+  }
 
   func activate(center: CGPoint, view: UIView) {
     let ripple = UIView()
@@ -21,20 +29,22 @@ class Ripple: NSObject {
     ripple.frame.origin = CGPoint(x: center.x - size.width / 2, y: center.y - size.height / 2)
     ripple.frame.size = size
     ripple.layer.borderColor = UIColor.whiteColor().CGColor
-    ripple.layer.borderWidth = 2
+    ripple.layer.borderWidth = 3
     ripple.layer.cornerRadius = size.width / 2
 
     let animation = CABasicAnimation(keyPath: "cornerRadius")
     animation.fromValue = ripple.layer.cornerRadius
     animation.toValue = size.width * multiplier / 2
-    animation.delegate = self
 
     let boundsAnimation = CABasicAnimation(keyPath: "bounds.size")
     boundsAnimation.fromValue = NSValue(CGSize: ripple.layer.bounds.size)
     boundsAnimation.toValue = NSValue(CGSize: CGSize(width: size.width * multiplier, height: size.height * multiplier))
 
+    let opacityAnimation = CAKeyframeAnimation(keyPath: "opacity")
+    opacityAnimation.values = [1, 1, 0]
+
     let animationGroup = CAAnimationGroup()
-    animationGroup.animations = [animation, boundsAnimation]
+    animationGroup.animations = [animation, boundsAnimation, opacityAnimation]
     animationGroup.duration = 1.5
     animationGroup.delegate = self
     animationGroup.timingFunction = CAMediaTimingFunction(controlPoints: 0.22, 0.54, 0.2, 0.47)
@@ -46,16 +56,9 @@ class Ripple: NSObject {
   override func animationDidStop(anim: CAAnimation, finished flag: Bool) {
     guard let ripple = ripples.first else { return }
 
-    ripple.layer.cornerRadius = size.width * multiplier / 2
-    ripple.bounds.size = CGSize(width: size.width * multiplier, height: size.height * multiplier)
-
-    UIView.animateWithDuration(0.15, animations: {
-      ripple.alpha = 0
-      }, completion: { _ in
-        ripple.layer.removeAnimationForKey("ripple")
-        ripple.removeFromSuperview()
-
-        self.ripples.removeFirst()
-    })
+    ripple.removeFromSuperview()
+    ripple.layer.removeAnimationForKey("ripple")
+    
+    ripples.removeFirst()
   }
 }
